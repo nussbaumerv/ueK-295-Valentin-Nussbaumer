90cf0c74a7e05e81154f92ddeed3978807274e30
Valentin Nussbaumer <valentin.nussbaumer@edu.tbz.ch>
2023-03-17 13:32:46 +0100

    added Project FIle

---
+++ uek295-Order/src/main/java/ch/noseryoung/uek295Order/Uek295OrderApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.uek295Order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uek295OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uek295OrderApplication.class, args);
+	}
+
+}
---
+++ uek295-Order/src/test/java/ch/noseryoung/uek295Order/Uek295OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.uek295Order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uek295OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
