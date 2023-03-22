87b1b188a1ee4fae6e1e50b7ec2975abf7bc97d6
Valentin Nussbaumer <valentin.nussbaumer@edu.tbz.ch>
2023-03-22 19:48:48 +0100

    removed unused parts of reposetory

--- Exercise/Exericse2/sbdemo01/src/main/java/ch/noseryoung/sbdemo01/domain/product/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO Test (description, price) VALUES ('hallo', 20.2);
--- uek295-Order/src/main/resources/data.sql
+++ uek295-Order/src/main/resources/data.sql
@@ -1,8 +1,10 @@
 INSERT INTO users (username, password) VALUES ('Valentin', '123456');
 INSERT INTO users (username, password) VALUES ('Valentin-le-admin', '654321');
+INSERT INTO users (username, password) VALUES ('Dude', '1234');
 
 INSERT INTO role (role_name) VALUES ('ADMIN');
 INSERT INTO role (role_name) VALUES ('USER');
+INSERT INTO role (role_name) VALUES ('NOTHING');
 
 INSERT INTO authority (authority_name) VALUES ('CREATE');
 INSERT INTO authority (authority_name) VALUES ('READ');
@@ -11,9 +13,10 @@ INSERT INTO authority (authority_name) VALUES ('DELETE');
 
 INSERT INTO user_role VALUES (1, 2);
 INSERT INTO user_role VALUES (2, 1);
+INSERT INTO user_role VALUES (3, 3);
 
 INSERT INTO role_authority VALUES (1, 1);
 INSERT INTO role_authority VALUES (1, 2);
 INSERT INTO role_authority VALUES (1, 3);
 INSERT INTO role_authority VALUES (1, 4);
-INSERT INTO role_authority VALUES (2, 1);
+INSERT INTO role_authority VALUES (2, 2);
